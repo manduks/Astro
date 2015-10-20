2af611f7fb25491d9d6b1377dfb7670d8bc75a89
Armando Gonzalez <iam@armando.mx>
2015-10-20 09:21:05 -0500

    Added support for centering lessons layout

--- views/lessons/LessonsLayout.jsx
+++ views/lessons/LessonsLayout.jsx
@@ -40,13 +40,13 @@ LessonsLayout = React.createClass({
         <Toolbar/>
         <section className="astro_main_container">
           <div className="astro_lessons_layout">
-            <div className="astro_lesson_course_info">
+            <aside className="astro_lesson_course_info">
                 <div className="astro_lesson_course_info_container">
                   <span className="astro_lesson_course_info_container_title">REACT</span>
                   <p>Aprende los principios basicos de esta asombrosa libreria</p>
                   <span className="astro_lesson_course_info_container_duration">1hr 30 min</span>
                 </div>
-            </div>
+            </aside>
             <LessonsList/>
           </div>
         </section>
