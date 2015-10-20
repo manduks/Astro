3692c70f4071f369f22f80fe0edc5a8bf10a0b3f
Armando Gonzalez <iam@armando.mx>
2015-10-19 22:53:55 -0500

    added support for the lessns layout

--- views/lessons/LessonItem.jsx
+++ views/lessons/LessonItem.jsx
@@ -2,7 +2,23 @@ LessonItem = React.createClass({
   render() {
     return (
       <div className="astro_lesson_item_wrapper">
-        {this.props.lesson.title}
+        <section className="astro_lesson_item_wrapper-inner">
+          <div className="astro_lesson_item_wrapper-inner-icon">
+            <img src={this.props.lesson.icon}></img>
+          </div>
+          <div className="astro_lesson_item_wrapper-inner-info">
+            <div className="astro_lesson_item_wrapper-inner-info-header">
+              <span>{this.props.lesson.title}</span>
+              <span>{this.props.lesson.duration}</span>
+            </div>
+            <span className="astro_lesson_item_wrapper-inner-info-description">
+              {this.props.lesson.description}
+            </span>
+          </div>
+        </section>
+        <div className="astro_lesson_item_wrapper-img">
+          <img src={this.props.lesson.image}></img>
+        </div>
       </div>
     )
   }
--- views/lessons/LessonsList.jsx
+++ views/lessons/LessonsList.jsx
@@ -5,27 +5,32 @@ LessonsList = React.createClass({
           title:'Que es react?',
           description:'Veamos que es React, una breve explication de que podemos hacer con esta libería.',
           duration:'9:34',
-          image: 'img/es6.png'
+          image: 'img/es6.png',
+          icon:'img/bookmark.png'
       },{
           title:'Que es react?',
           description:'Veamos que es React, una breve explication de que podemos hacer con esta libería.',
           duration:'9:35',
-          image: 'img/es6.png'
+          image: 'img/es6.png',
+          icon:'img/check.png'
       },{
           title:'Que es react?',
           description:'Veamos que es React, una breve explication de que podemos hacer con esta libería.',
           duration:'9:36',
-          image: 'img/es6.png'
+          image: 'img/es6.png',
+          icon:'img/bookmark.png'
       },{
           title:'Que es react?',
           description:'Veamos que es React, una breve explication de que podemos hacer con esta libería.',
           duration:'9:37',
-          image: 'img/es6.png'
+          image: 'img/es6.png',
+          icon:'img/check.png'
       },{
           title:'Que es react?',
           description:'Veamos que es React, una breve explication de que podemos hacer con esta libería.',
           duration:'9:38',
-          image: 'img/es6.png'
+          image: 'img/es6.png',
+          icon:'img/bookmark.png'
       }]
     };
   },
