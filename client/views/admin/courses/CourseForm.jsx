924b7c0af735d26b102dfbe610cf031f3daf811b
Luis Enrique Martínez Gómez <lumartineck@gmail.com>
2015-12-30 00:09:06 -0600

    obtain values course form

--- client/views/admin/courses/CourseForm.jsx
+++ client/views/admin/courses/CourseForm.jsx
@@ -6,6 +6,37 @@ CourseForm = React.createClass({
       files: S3.collection.find().fetch()
     }
   },
+  getInitialState: function() {
+    return {title: '', file: '', description: '', lessonNumber: '', duration: ''};
+  },
+  handleTitleChange: function(e) {
+    this.setState({title: e.target.value});
+  },
+  handleDescriptionChange: function(e) {
+    this.setState({description: e.target.value});
+  },
+  handleLessonNumberChange: function(e) {
+    this.setState({lessonNumber: e.target.value});
+  },
+  handleDurationChange: function(e) {
+    this.setState({duration: e.target.value});
+  },
+  uploadCourseImageChange: function(e) {
+    var self = this,
+      files =  this.refs.imageInput.files;
+    S3.upload({
+      files: files,
+      path : 'courses_assets'
+    },function(err,r){
+      console.log(arguments);
+      if (err) {
+        return console && console.log(err);
+      }
+      //console.log(r);
+      self.fileURL = r.url;
+      self.setState({file: r.url});
+    });
+  },
   render() {
     let file = this.data.files[this.data.files.length - 1];
     let imageInputLabel = file ? (file.percent_uploaded + ' %') : 'Elegir imagen';
@@ -17,21 +48,21 @@ CourseForm = React.createClass({
       <div className="astro_form_component">
         <form className ="astro_form_component_content" onSubmit={this.addOrUpdateCourse}>
           <div className="astro_form_component_content_textfield2">
-            <input type="text" name="title" defaultValue="" autoComplete="off" placeholder="Título del curso" required/>
+            <input type="text" name="title" value={this.state.title} onChange={this.handleTitleChange} autoComplete="off" placeholder="Título del curso" required/>
           </div>
           <div className="astro_form_component_content_textfield2">
-            <textarea rows="4" cols="50" name="description" autoComplete="off" placeholder="Descripción" required/>
+            <textarea rows="4" cols="50" name="description" value={this.state.description} onChange={this.handleDescriptionChange} autoComplete="off" placeholder="Descripción" required/>
           </div>
           <div className="astro_form_component_content_textfield2">
-            <input type="text" name="duration" defaultValue="" autoComplete="off" placeholder="Número de lecciones" required/>
+            <input type="text" name="lessonNumber" value={this.state.lessonNumber} onChange={this.handleLessonNumberChange} autoComplete="off" placeholder="Número de lecciones" required/>
           </div>
           <div className="astro_form_component_content_textfield2">
-            <input type="text" name="duration" defaultValue="" autoComplete="off" placeholder="Duración" required/>
+            <input type="text" name="duration" value={this.state.duration} onChange={this.handleDurationChange} autoComplete="off" placeholder="Duración" required/>
           </div>
           <div className="astro_form_component_content_textfield2">
             <div className="file_input_wrapper">
               <div className="file_upload_label">{imageInputLabel}</div>
-              <input type="file" name="upload" ref="imageInput" onChange={this.uploadCourseImage} className="upload_field" title="Elegir imagen"/>
+              <input type="file" name="upload" ref="imageInput" onChange={this.uploadCourseImageChange} className="upload_field" title="Elegir imagen"/>
             </div>
           </div>
           <input type="submit" className="astro_button large" value= "Guardar"/>
@@ -39,20 +70,18 @@ CourseForm = React.createClass({
       </div>
     )
   },
-  uploadCourseImage() {
-    var self = this,
-      files =  this.refs.imageInput.files;
-    S3.upload({
-      files: files,
-      path : 'courses_assets'
-    },function(e,r){
-      console.log(arguments);
-      if (e) {
-        return console && console.log(e);
-      }
-      //console.log(r);
-      self.fileURL = r.url;
-    });
+  addOrUpdateCourse(e) {
+    e.preventDefault();
+    var title = this.state.title.trim();
+    var description = this.state.description.trim();
+    var lessonNumber = this.state.lessonNumber.trim();
+    var duration = this.state.duration.trim();
+
+    console.log(this.state);
+    console.log(self.fileURL);
+    //TODO: validations
+    // TODO: send request to save the server
+    this.setState({title: '', file: '', description: '', lessonNumber: '', duration: ''});
   }
 });
 
